91a6dfeabdefc5c7cabb2732513462de55ff38bd
ADMIN-PC$ <patelkr120@gmail.com>
2021-07-17 11:25:57 +0530

    Project is commited by Krishna

---
+++ GitandGitGubEclipse/src/GitusingEclipse/testcase1.java
@@ -0,0 +1,13 @@
+package GitusingEclipse;
+
+public class testcase1 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is testCase 1");
+
+	}
+
+}
